4ad6333396f830d661fe728e4a98ef1ed5bee5bf
socassergio94 <socassergio94@gmail.com>
2025-10-21 13:05:28 +0100

    Fix: ruta del CSS en BaseLayout 3.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
+import github from '@astrojs/github';
 
 export default defineConfig({
-  site: "https://socassergio94.github.io/mi-portafolio-sergio/",
-  base: "/mi-portafolio-sergio/",
-  viewTransitions: true,
+  output: 'static',
+  integrations: [github()],
 });
