cc1f4b21881b1d1936b55dd8e6373607272fa56f
socassergio94 <socassergio94@gmail.com>
2025-10-21 14:48:40 +0100

    cambio 4.5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 import { defineConfig } from 'astro/config';
-import github from '@astrojs/github';
 
 export default defineConfig({
   output: 'static',
-  integrations: [github()],
-  site: "https://socassergio94.github.io",
-base: "mi-portafolio-sergio"
-
+  site: 'https://socassergio94.github.io',
+  base: '/mi-portafolio-sergio/',
 });
