161c4856680e8af670e04978224cf755cc2d1f1e
socassergio94 <socassergio94@gmail.com>
2025-10-21 10:24:48 +0100

    Fix base path for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://socassergio94.github.io/mi-portafolio-sergio/",
+  base: "/mi-portafolio-sergio/",
   viewTransitions: true,
 });
