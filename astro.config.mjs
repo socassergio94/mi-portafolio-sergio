9228c5371f58b1e246056f92e9ae35e4d630d497
socassergio94 <socassergio94@gmail.com>
2025-10-22 11:36:19 +0100

    Forzar re-deploy GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,7 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: "https://socassergio94.github.io/mi-portafolio-sergio/",
-  base: "/mi-portafolio-sergio/",
-  outDir: "./dist",
-  trailingSlash: "always",
-  build: {
-    format: "directory",
-  },
-  viewTransitions: true,
+  output: 'static',
+  site: 'https://socassergio94.github.io',
+ 
 });
