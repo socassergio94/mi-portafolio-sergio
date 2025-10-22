b8cfff529a1aa5b149a88dea01de248ec19e0160
socassergio94 <socassergio94@gmail.com>
2025-10-22 12:00:35 +0100

    el codigo va a acabar conmigo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://socassergio94.github.io/mi-portafolio-sergio/",
+  site: "https://socassergio94.github.io",
   base: "mi-portafolio-sergio", 
   outDir: "./dist",
   viewTransitions: true,
